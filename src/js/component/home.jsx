fe1e7c71a9064998b98259ef7a2334518746b885
Leilani Tanimoto Abad <135250913+Leilanta@users.noreply.github.com>
2023-11-13 19:19:07 +0000

    actualizacion

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,36 +7,56 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	const [inputValue,setInputValue] = useState ("");
 	const [todos,setTodos] = useState ([]);
+	const deleteTodo = (index) => {
+		const newTodos = todos.filter((_, i) => i !== index);
+		setTodos(newTodos);
+	  };
+	const [item, setItem ] = useState('items');
 
 	return (
-		<div className="container m-5 shadow">
-			<h1 className="d-flex justify-content-center mb-5">Cosas qué hacer:</h1>
+	
+		<div className="container justify-content-center bg-light rounded-3 p-5 m-5 shadow">
+			<h1 className="d-flex justify-content-center mb-5">To-do's</h1>
 			<div className="row justify-content-center">
 			<ul className="col justify-content-center">
 				<li>
-				<input
-					type="text"
+				<textarea
+
 					onChange={(e)=>setInputValue(e.target.value)}
 					value={inputValue}
 					onKeyDown={(e) => {
 						if (e.key === "Enter"){
 							setTodos(todos.concat(inputValue));
 							setInputValue ("");
+						} else if (todos.length === 0) {
+								setItem(" tasks, add a task")
+						} else {
+								setItem('items')
+							}
 						}
-					}}
-					placeholder="¿Qué vamos a procrastinar hoy?">
-					</input>
+					}
+					placeholder="You go, girl!">
+
+					</textarea>
 				</li>
-				{todos.map((item) => (
-				<li>
-					{item} <a>🗑️</a>
+				{todos.map((item,index) => (
+				<li
+				className="submittedTodos">
+					{item} <a
+					className="icono d-flex justify-content-end"
+					onClick={() => deleteTodo(index)}
+					>🗑️</a>
 					{""}	
 				</li>
 				))}
 			</ul>
-			<p className="mp-5">{todos.length} items left</p>
+			<p
+			
+			> {todos.length} {item}</p>
 			</div>
 		</div>
+	
+	
 	);
 };
 
